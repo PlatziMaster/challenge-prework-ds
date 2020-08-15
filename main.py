67c70b5bde7b3391cadd477e1268f2fffc8c1be7
joethecoderr <joel.e.gaspar@gmail.com>
2020-08-14 23:20:06 -0600

    leyendo csv y mostrandolo en pantalla. Hay que limpiar datos

--- main.py
+++ main.py
@@ -2,18 +2,36 @@ import tkinter as tk
 from tkinter.filedialog import askopenfile
 
 import pandas as pd
-def button_load_csv_clicked(txt_box):
+
+
+
+
+
+def button_load_csv_clicked( window):
     file_name = askopenfile(
                            filetypes =(("CSV File", "*.csv"),("All Files","*.*")),
                            title = "Choose a file."
                            )
     
-    txt_box.Text("HOLA")
+    data = pd.read_csv(file_name, sep=',') 
+
+
+    data_frame = pd.DataFrame(data)
+
+    #root = window
+
+    table = tk.Text()
+    table.place(x=250, y=200)
+    table.insert(tk.INSERT, data_frame.to_string())
+    
+    #table.pack() 
+    """
     try:
         with open(file_name,'r') as CSVFile:
             pass
     except:
         print("No existe el archivo")
+        """
 
 def run(window):
     
@@ -21,9 +39,8 @@ def run(window):
     window.geometry("1000x1000")
     lbl = tk.Label( text = "Estadisticas del INAH", font=("Arial Bold", 26))
     lbl.place(x=390, y=0)
-    txt_box = tk.Text("Hola")
-    txt_box.place(x=390, y=100)
-    btn_load_csv = tk.Button( text="Buscar archivo", bg="green", fg="white", font=("Arial Bold", 18), command=lambda:button_load_csv_clicked(txt_box))
+
+    btn_load_csv = tk.Button( text="Buscar archivo", bg="green", fg="white", font=("Arial Bold", 18), command=lambda:button_load_csv_clicked(window))
     #btn_load_csv = tk.Button( text="Buscar archivo", bg="green", fg="white", font=("Arial Bold", 18), command=button_load_csv_clicked)
     btn_load_csv.place(x=460, y=60)
     
