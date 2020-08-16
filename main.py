6f7e7b93814cf2dcd6ad287d278311f7611c195c
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-16 15:24:53 -0500

    Partial solution

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+## In this first part we import the libraries for our proyect. We name our panda library as pd in order to make it managable.
+
 import csv
-import pandas as database
-import plotly.express as px
+import pandas as pd
 
 def read():
+
+## This function has the only purpose of opening the data on a raw fashion in the terminal. In this case we use the .DictReader as our main
+## method to do so and a simple for loop to iterate through each of the lines in the csv file. 
 
         with open("INAH_detallado_2019.csv", mode="r") as csv_file:
                 csv_reader = csv.DictReader(csv_file)
@@ -14,17 +18,32 @@ def read():
 
 def clean():
 
-        clean_db = database.read_csv ("INAH_detallado_2019.csv", encoding = "ISO-8859-1")
+        ## Using the pandas library, we take the raw data from the last function, clean it and make it more presentable.
+        ## Is important to keep the encoding variable in order to avoid an error while running the code. 
+        ## Quick note: This is the first time I ever use pandas. 
+
+        clean_db = pd.read_csv ("INAH_detallado_2019.csv", encoding = "ISO-8859-1")
         print(clean_db)
 
 def analytics():
 
-        clean_db = database.read_csv ("INAH_detallado_2019.csv", encoding = "ISO-8859-1")
-        chart = px.data()
+        ## The anaylitics function has the only purpose of presenting organized data. In this case, I focused only on grouping and counting
+        ## the total number of visitors on each state -- as well as the nationality. 
+        ## Here you will only find a simple table which will be visible in the terminal. 
 
-        visitors = px.bar(chart, x="")
+        clean_db = pd.read_csv ("INAH_detallado_2019.csv", encoding = "ISO-8859-1")
+        columns = clean_db.columns
+        groupby_state = clean_db.groupby("Estado")["Número de visitas"].count()
+        groupby_nationality = clean_db.groupby("Nacionalidad")["Número de visitas"].count()
+
+        print(groupby_state)
+        print(groupby_nationality)
 
 def run():
+
+        ## Our run function contains a simple menu in order to let the use to easily decide what action to execute. 
+        ## We keep it on a while loop, so that the program keeps running even after executing one of the four options available. 
+        ## It's important to keep the .upper method, so that the menu options run smoothley no matter if the user type mayus or minus keys.
 
     while(True):
 
@@ -51,10 +70,13 @@ def run():
                 elif action == "E":
                         quit()
                 else:
-                        while action != "R" or "E":
+                        while action != "R" or "O" or "S" or "E":
                                 new_action = input("That's an invalid option. Press any key to try again ")
                                 break
 
 
 if __name__ == "__main__":
     run()
+
+    ## Here we only invoke our run function to start our program.
+    ## Thank you for reviewing my code. Looking forward to become a legendary data scientist. 
